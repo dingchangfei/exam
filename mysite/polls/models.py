826ec1c0e98d0538564e67f029e18bccf6eaa07e
Changfei.Ding <changfei.ding@pfizer.com>
2017-03-02 11:34:44 +0800

    migrate for new modesl

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    qusertion_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    vates = models.IntegerField(default=0)
